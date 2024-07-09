44d7c4e001a622e1c9be616715ee5b3b8b74f29a
manyanzhang <manyanzhang8@gmail.com>
2024-07-09 16:36:59 +0800

    add commander

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
-
 //标识文件使用node执行
-console.log("success!!!");
+
+import { Command } from "commander";
+import Pack from "../package.json";
+
+const program = new Command();
+
+program
+  .name(Pack.name)
+  .description("A simple CLI")
+  .version(Pack.version)
+  .helpOption("-h, --HELP", "read more information")
+  .usage("<command> [option]");
+
+program.parse(process.argv);
