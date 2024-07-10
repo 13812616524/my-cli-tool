3c5217709b7cdaedbe2b816f448e76c3845edc20
manyanzhang <manyanzhang8@gmail.com>
2024-07-10 13:57:42 +0800

    add inquirer

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 //标识文件使用node执行
 
 import { Command } from "commander";
+import inquirer from "inquirer";
 import Pack from "../package.json";
 
 const program = new Command();
@@ -12,5 +13,82 @@ program
   .version(Pack.version)
   .helpOption("-h, --HELP", "read more information")
   .usage("<command> [option]");
+
+program.command("choose").action(() => {
+  inquirer
+    .prompt([
+      {
+        type: "checkbox",
+        message: "Select toppings",
+        name: "toppings",
+        choices: [
+          new inquirer.Separator(" = The Meats = "),
+          {
+            name: "Pepperoni",
+            value: "pepperoni",
+          },
+          {
+            name: "Ham",
+            value: "ham",
+          },
+          {
+            name: "Ground Meat",
+            value: "ground Meat",
+          },
+          {
+            name: "Bacon",
+            value: "bacon",
+          },
+          new inquirer.Separator(" = The Cheeses = "),
+          {
+            name: "Mozzarella",
+            value: "mozzarella",
+            checked: true,
+          },
+          {
+            name: "Cheddar",
+            value: "cheddar",
+          },
+          {
+            name: "Parmesan",
+            value: "parmesan",
+          },
+          new inquirer.Separator(" = The usual ="),
+          {
+            name: "Mushroom",
+            value: "mushroom",
+          },
+          {
+            name: "Tomato",
+            value: "tomato",
+          },
+          new inquirer.Separator(" = The extras = "),
+          {
+            name: "Pineapple",
+            value: "pineapple",
+          },
+          {
+            name: "Olives",
+            disabled: "out of stock",
+            value: "olives",
+          },
+          {
+            name: "Extra cheese",
+            value: "extra cheese",
+          },
+        ],
+        validate(answer) {
+          if (answer.length === 0) {
+            return "You must choose at least one topping.";
+          }
+
+          return true;
+        },
+      },
+    ])
+    .then((answers) => {
+      console.log(JSON.stringify(answers, null, "  "));
+    });
+});
 
 program.parse(process.argv);
